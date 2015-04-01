65330112e3b33248f8f9991ae389f0a57af049b9
tabbara <abbara@gmail.com>
2015-04-01 09:45:03 +0200

    test

--- fb.js
+++ fb.js
@@ -40,7 +40,7 @@ window.fbAsyncInit = function() {
       // Otherwise, show Login dialog first.
       FB.login(function(response) {
         onLogin(response);
-      }, {scope: 'user_friends, email, user_birthday'});
+      }, {scope: 'user_friends, email'});
     }
   });
 
